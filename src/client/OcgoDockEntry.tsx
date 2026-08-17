3bfe73175abdd722dca1988e07ef21223d87616e
Shawn <shawnhui1012@gmail.com>
2026-08-17 16:49:27 +0800

    chore: align dark logo colors with final asset revision

--- src/client/OcgoDockEntry.tsx
+++ src/client/OcgoDockEntry.tsx
@@ -126,10 +126,10 @@ function OcgoLogo(): React.ReactElement {
     return (
       <svg className={css.logo} width="22" height="12" viewBox="0 0 54 30" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
         <rect width="100%" height="100%" fill="#2c2c2e" />
-        <path d="M24 30H0V0H24V6H6V24H18V18H12V12H24V30Z" fill="#B0B0B0" />
-        <path d="M12 18H18V24H6V12H12V18Z" fill="#2c2c2e" />
-        <path d="M48 12V24H36V12H48Z" fill="#2c2c2e" />
-        <path d="M54 30H30V0H54V30ZM36 24H48V6H36V24Z" fill="#B0B0B0" />
+        <path d="M24 30H0V0H24V6H6V24H18V18H12V12H24V30Z" fill="#e6edf3" />
+        <path d="M12 18H18V24H6V12H12V18Z" fill="#646464" />
+        <path d="M48 12V24H36V12H48Z" fill="#646464" />
+        <path d="M54 30H30V0H54V30ZM36 24H48V6H36V24Z" fill="#e6edf3" />
       </svg>
     )
   }
